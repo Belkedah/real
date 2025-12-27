05cee1881ce70c8aa5690ab4f6f6e7846e3c0fad
Belkedah <ilybelkedah@gmail.com>
2025-12-27 14:59:09 +0100

    Update NetworkHandler.java

--- src/main/java/com/belkedouch/raidmod/network/NetworkHandler.java
+++ src/main/java/com/belkedouch/raidmod/network/NetworkHandler.java
@@ -1,21 +1,23 @@
 package com.belkedouch.raidmod.network;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
 
 public class NetworkHandler {
-    private static final String PROTOCOL_VERSION = "1";
-    public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(
-        net.minecraft.resources.ResourceLocation location = new net.minecraft.resources.ResourceLocation("raidmod", "main");
+    public static final String PROTOCOL_VERSION = "1";
+
+    public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
+        new ResourceLocation("raidmod", "main"),
         () -> PROTOCOL_VERSION,
         PROTOCOL_VERSION::equals,
         PROTOCOL_VERSION::equals
     );
 
-    public static void init() {
-        // Network messages can be registered here if needed
-    }
+    // Message registration example
+    // public static void registerMessages() { ... }
 }
+
 
 
 
